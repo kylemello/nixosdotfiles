c7a9b4e48813e4cb364ed75eaa1bf79df2f5aba7
kylemello <kylemello98@gmail.com>
2025-08-23 21:50:02 -0400

    Added .ssh/config

--- home/profiles/desktop.nix
+++ home/profiles/desktop.nix
@@ -15,10 +15,121 @@
 
   programs.ssh = {
     extraConfig = ''
-      Include ~/.ssh/1Password/config 
+      # ================
+      # General Settings
+      # ================
+      Include ~/.ssh/1Password/config
+      ForwardX11 no
 
+      # For 1password-cli
       Host *
-        IdentityAgent "~/.1password/agent.sock"
+        ForwardAgent yes
+        IdentitiesOnly yes
+
+      # =======================
+      # Friend Hosts
+      # =======================
+      Host cdbits
+        HostName cdbits.xyz
+        User kmello
+        Port 2520
+
+      # =======================
+      # Home Hosts
+      # =======================
+      Host atlas
+        HostName 10.11.12.104
+        User kyle
+
+      Host newatlas
+        HostName 172.19.1.21
+        User kyle
+
+      Host sputnik
+        HostName 10.11.12.5
+        User root
+
+      Host artemis
+        HostName 10.11.12.102
+        User kylem
+
+      Host ariane
+        HostName ariane
+        User kyle
+
+      Host github.com
+        HostName github.com
+        User git
+
+      Host gitea.kmello.dev
+        HostName gitea.kmello.dev
+        Port 2222
+        User git
+
+      Host opnsense
+        HostName 10.11.12.1
+        User kly108
+
+      # =======================
+      # Broad River Rehab Hosts
+      # =======================
+
+      Host mercury
+        HostName brrit.com
+        User kyle
+
+      Host lawrencium
+        HostName docresolve.com
+        User kyle
+
+      Host thulium
+        HostName 52.142.49.5
+        User kyle
+
+      Host iodine
+        HostName 10.20.23.241
+        User kmello
+
+      Host gitlab.jamsit.dev
+        HostName gitlab.jamsit.dev
+        User git
+        # Port 522
+
+      Host uranium
+        HostName 10.20.23.230
+        User kmello
+
+      Host polonium
+        HostName 10.20.23.232
+        User kmello
+
+      Host plutonium
+        HostName brr.docresolve.com
+        User kyle
+
+      Host lead
+        HostName 10.20.23.240
+        User root
+
+      Host neodymium
+        HostName 57.151.116.136
+        User kyle
+
+      Host vega
+        HostName 10.20.23.254
+        user jamsit-sa
+
+      Host nitrogen
+        HostName 10.20.23.243
+        user kmello
+
+      Host mageai
+        HostName mage-iitzd-u20494.vm.elestio.app
+        User root
+
+      Host strontium
+        HostName dev.brrit.com
+        User kyle
     '';
   };
 }
